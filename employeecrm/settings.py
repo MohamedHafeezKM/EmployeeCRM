f4211300a00eec1a1bc6af581625a0145703831b
MohamedHafeezKM <hmohamedhafeez@gmail.com>
2023-11-17 13:41:10 +0530

    second commit

--- employeecrm/settings.py
+++ employeecrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ilp8w+$8oj-%pp0xmsu!p$66t7vhn1d7hr6ac=l*qrgvjgtkx!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']  
 
 
 # Application definition
